c73ce7aff3b9f9e25fc43aa7e2015d9c0ec05fe9
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-01-29 12:46:23 +0000

    Update CheckTime.php

--- VoidMute/src/VoidMute/CheckTime.php
+++ VoidMute/src/VoidMute/CheckTime.php
@@ -17,7 +17,7 @@
 	along with this program.  If not, see (http://www.gnu.org/licenses/). 
 */
 
-namespace RealMute;
+namespace VoidMute;
 
 use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\Config;
